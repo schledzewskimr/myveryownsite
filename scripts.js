d7c0219cd643745cbfc6543d475ebf2a2f65941f
Mark Schledzewski <mark.schledzewski@gmail.com>
2022-03-18 09:22:10 -0400

    made a fourth 'secret' page

--- scripts.js
+++ scripts.js
@@ -5,3 +5,12 @@ document.addEventListener("DOMContentLoaded", (e)=>{
         splash.classList.add("display-none");
     }, 2000);
 });
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+})
